86d09d925df7e96ab6fd6644b6512ced314be84f
podisto <omardione1991@gmail.com>
2018-04-10 12:23:10 +0000

    Init angular calendar https://github.com/mattlewis92/angular-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CalendarModule } from 'angular-calendar';
 import { AppComponent } from './app.component';
-
+import { NgbModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoUtilsModule } from './demo-utils/demo-utils.module';
+import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    NgbModule.forRoot(),
+    NgbModalModule.forRoot(),
+    DemoUtilsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
